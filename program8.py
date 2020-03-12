66da6decbbb30d9ae39da4c719f53379d8b88ccb
Dosenji <61090023@kmitl.ac.th>
2020-03-12 17:19:09 +0700

    stopfish

--- program8.py
+++ program8.py
@@ -9,7 +9,7 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.rabbit = QPixmap("rabbit.png")
 
     def paintEvent(self, e):
         p = QPainter()
@@ -28,6 +28,31 @@ class Simple_drawing_window(QWidget):
         p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400)])
         p.drawPixmap(QRect(200,100,320,329), self.rabbit)
         p.end()
+
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Simple Drawing")
+        self.fish = QPixmap("fish.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon(
+            [QPoint(70,100),QPoint(100,110),QPoint(130,100),QPoint(100,150)]
+        )
+
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+
+        p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400)])
+        p.drawPixmap(QRect(200,100,320,329), self.rabbit)
+        p.end()
+
 
 def main():
     app = QApplication(sys.argv)
