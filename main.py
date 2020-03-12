a95aca8694d661913f899ea4a191de18c8c2bdc1
Dosenji <61090023@kmitl.ac.th>
2020-03-12 18:00:06 +0700

    fixed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from program8 import *
+from Simple_drawing_window2 import *
 
 def main():
     app = QApplication(sys.argv)
 
-    w= Simple_drawing_window()
+    w= Simple_drawing_window2()
     w.show()
     return app.exec_()
 
